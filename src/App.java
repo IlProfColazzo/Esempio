a48e268ab6dc6060d2da590123f0ca4a22c3acba
Luciano <Luciano@Luciano.it>
2024-11-08 11:53:15 +0100

    primocommit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println("La mia prima modifica!");
         System.out.println("Hello, World!");
     }
 }
